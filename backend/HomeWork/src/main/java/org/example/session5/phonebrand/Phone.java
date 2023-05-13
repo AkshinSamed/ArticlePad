7a2edcec42a15a9ef0184cf1435ef7d3e9ebdf36
AkshinSamed <samedliakshin@gmail.com>
2023-05-13 19:34:42 +0400

    frontend and backend configuration

--- backend/HomeWork/src/main/java/org/example/session5/phonebrand/Phone.java
+++
@@ -1,7 +0,0 @@
-package org.example.session5.phonebrand;
-
-public interface Phone {
-    public void takePicture();
-
-    public void printColor();
-}
--- backend/Session4/src/main/java/session5/phonebrand/Phone.java
+++
@@ -1,6 +0,0 @@
-package session5.phonebrand;
-
-public interface Phone {
-    public void takePicture();
-    public void printColor();
-}
